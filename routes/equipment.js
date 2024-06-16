2e91573f7a5be8a307abfe3e93e124476ff9deb1
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-06-16 14:48:32 -0700

    Added equipment route file to define routes to handle CRUD operations

---
+++ routes/equipment.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+const equipment = require('../data/equipment.js');
+
+//GET route to get all equipmentItem data
+router.get('/', (req, res) => {
+  const links = [
+    {
+      href: 'equipment/:id',
+      rel: ':id',
+      type: 'GET',
+    },
+  ];
+
+  res.json({ equipment, links });
+});
+
+// GET router to get a equipmentItem by ID
+router.get('/:id', (req, res, next) => {
+  // Using the Array.find method to find the equipmentItem with the same id as the one sent with the request
+  const equipmentItem = equipment.find((u) => u.id == req.params.id);
+
+  const links = [
+    {
+      href: `/${req.params.id}`,
+      rel: '',
+      type: 'PATCH',
+    },
+    {
+      href: `/${req.params.id}`,
+      rel: '',
+      type: 'DELETE',
+    },
+  ];
+
+  if (equipmentItem) res.json({ equipmentItem, links });
+  else next();
+});
+
+// POST Create a equipmentItem
+router.post('/', (req, res) => {
+  // Within the POST request we will create a new equipmentItem.
+  // The client will pass us data and we'll push that data into our equipment array.
+  // the equipmentItem data that we want to create is inside the req.body
+  if (req.body.name && req.body.equipmentItemname && req.body.email) {
+    if (equipment.find((u) => u.equipmentItemname === req.body.equipmentItemname)) {
+      // The above returns an object, we found an existing equipmentItem with the same equipmentItemname. So it's a no go
+      res.json({ error: 'equipmentItemname Already Taken' });
+      return;
+    }
+
+    // If the code gets to this point, we are good to create the equipmentItem
+    const equipmentItem = {
+      id: equipment.length + 1,
+      name: req.body.name,
+      equipmentItemname: req.body.equipmentItemname,
+      email: req.body.email,
+    };
+
+    equipment.push(equipmentItem);
+    res.json(equipmentItem);
+  } else {
+    res.status(400).json({ error: 'Insufficient Data' });
+  }
+});
+
+//PATCH Update a equipmentItem
+router.patch('/:id', (req, res, next) => {
+  // Within the PATCH request route, we allow the client
+  // to make changes to an existing equipmentItem in the database.
+  const equipmentItem = equipment.find((u, i) => {
+    if (u.id == req.params.id) {
+      for (const key in req.body) {
+        // Applying the updates within the req.body to the in-memory equipmentItem
+        equipment[i][key] = req.body[key];
+      }
+      return true;
+    }
+  });
+
+  if (equipmentItem) {
+    res.json(equipmentItem);
+  } else {
+    next();
+  }
+});
+
+// Delete a equipmentItem
+router.delete('/:id', (req, res) => {
+  // The DELETE request route simply removes a resource.
+  const equipmentItem = equipment.find((u, i) => {
+    if (u.id == req.params.id) {
+      equipment.splice(i, 1);
+      return true;
+    }
+  });
+
+  if (equipmentItem) res.json(equipmentItem);
+  else next();
+});
+
+module.exports = router;
